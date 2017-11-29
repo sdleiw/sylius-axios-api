403ef245143fdbd300ef0d11af3cf94fa3cf082c
Lei Wang <wang@solutiondrive.de>
2017-11-29 23:41:37 +0100

    add review api + tests

---
+++ test/api/reviewApi.spec.js
@@ -0,0 +1,59 @@
+import axios from 'axios'
+import MockAdapter from 'axios-mock-adapter'
+import { expect } from 'chai'
+import appConfig from '../../src/config'
+
+import reviewApi from '../../src/api/reviewApi'
+
+describe('api/reviewApi', () => {
+    let mock
+
+    before(() => {
+        mock = new MockAdapter(axios)
+    })
+
+    afterEach(() => {
+        mock.reset()
+    })
+
+    after(() => {
+        mock.restore()
+    })
+
+    const testReviewResponse = {review: '1'}
+
+    it('gets review by slug', () => {
+        const slug = 'testSlug'
+        mock.onGet(appConfig.apiUrl + '/product-reviews-by-slug/' + slug + '?channel=' + appConfig.apiChannel)
+            .reply(200, testReviewResponse)
+
+        return reviewApi.getBySlug(slug)
+            .then(response => {
+                expect(response.status).to.equal(200)
+                expect(response.data).to.deep.equal(testReviewResponse)
+            })
+    })
+
+    it('adds review by slug', () => {
+        const slug = 'testSlug'
+        const title = 'testReview'
+        const rating = '3'
+        const comment = 'no comment'
+        const email = 'unit@test.de'
+        const data = {
+            channelCode: appConfig.apiChannel,
+            title,
+            rating: parseInt(rating),
+            comment,
+            email
+        }
+        mock.onPost(appConfig.apiUrl + '/product-reviews-by-slug/' + slug, data)
+            .reply(200, testReviewResponse)
+
+        return reviewApi.addBySlug(slug, title, rating, comment, email)
+            .then(response => {
+                expect(response.status).to.equal(200)
+                expect(response.data).to.deep.equal(testReviewResponse)
+            })
+    })
+})
