28adf9354ceb17a0233a206e69b3408d69261a60
Lei Wang <wang@solutiondrive.de>
2017-12-01 23:46:46 +0100

    coupon api

--- src/api/couponApi.js
+++ src/api/couponApi.js
@@ -1,10 +1,29 @@
 import axios from 'axios'
 import appConfig from '../../src/config'
 
+/**
+ * Coupon API: add and remove coupon for the given cart id
+ * @module api/coupon
+ */
 export default {
+
+    /**
+     * PUT | apply coupon
+     *
+     * @param {string} cartid cart id
+     * @param {string} coupon coupon
+     * @returns {AxiosPromise} 200 | cart object
+     */
     add (cartid, coupon) {
         return axios.put(appConfig.apiUrl + '/carts/' + cartid + '/coupon', {coupon})
     },
+
+    /**
+     * DELETE | remove coupon
+     *
+     * @param {string} cartid cart id
+     * @returns {AxiosPromise} 200 | cart object
+     */
     remove (cartid) {
         return axios.delete(appConfig.apiUrl + '/carts/' + cartid + '/coupon')
     }
