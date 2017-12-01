b79c9a246c7bfb2575f4ecfdc1c968b39e7f1086
Lei Wang <wang@solutiondrive.de>
2017-12-01 23:32:02 +0100

    category api

--- src/api/categoryApi.js
+++ src/api/categoryApi.js
@@ -1,11 +1,26 @@
 import axios from 'axios'
 import appConfig from './../config'
 
+/**
+ * Category API: fetch all Categories and fetch category by Code
+ * @module api/category
+ */
 export default {
+
+    /**
+     * GET | fetch taxons
+     *
+     * @returns {AxiosPromise} 200 | category tree
+     */
     getAll () {
         return axios.get(appConfig.apiUrl + '/taxons/')
     },
-
+    /**
+     * GET | fetch category by code
+     *
+     * @param {string} code taxon code
+     * @returns {AxiosPromise} 200 | category object
+     */
     getByCode (code) {
         return axios.get(appConfig.apiUrl + '/taxons/' + code + '?locale=' + appConfig.apiLocale)
     }
