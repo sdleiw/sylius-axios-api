2bc74623c1425e5672583b26a990e510718eeb33
Lei Wang <wang@solutiondrive.de>
2017-12-01 23:28:16 +0100

    add doc for review api

--- src/api/reviewApi.js
+++ src/api/reviewApi.js
@@ -1,22 +1,32 @@
 import axios from 'axios'
 import appConfig from '../../src/config'
 
+/**
+ * Review API: fetch and add Review to product by slug
+ * @module api/review
+ */
 export default {
+
     /**
-     * @param slug
-     * @returns {AxiosPromise}
+     * GET | fetch Review by Product Slug of a channel
+     *
+     * @todo Channel parameter as dependency
+     * @param {string} slug product slug
+     * @returns {AxiosPromise} 200 | review object
      */
     getBySlug (slug) {
         return axios.get(appConfig.apiUrl + '/product-reviews-by-slug/' + slug + '?channel=' + appConfig.apiChannel)
     },
 
     /**
-     * @param slug
-     * @param title
-     * @param rating
-     * @param comment
-     * @param email
-     * @returns {AxiosPromise}
+     * POST | add review to product by its slug
+     *
+     * @param {string} slug product slug
+     * @param {string} title review title
+     * @param {string} rating review rating
+     * @param {string} comment review comment
+     * @param {string} email customer email
+     * @returns {AxiosPromise} 201 | no content
      */
     addBySlug (slug, title, rating, comment, email) {
         const data = {
