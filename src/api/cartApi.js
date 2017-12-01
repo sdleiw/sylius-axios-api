39f2d7519c4654b48c70373003074cbf1df00a5a
Lei Wang <wang@solutiondrive.de>
2017-12-01 23:28:33 +0100

    add doc for cart api

--- src/api/cartApi.js
+++ src/api/cartApi.js
@@ -1,7 +1,21 @@
 import axios from 'axios'
 import appConfig from '../../src/config'
 
+/**
+ * cart api includes
+ * pickup fetch and remove of cart object
+ * add, update and remove of each cart item
+ *
+ * @module api/cart
+ */
 export default {
+
+    /**
+     * POST | pickup a cart before add item to it, needs to send a uuid
+     *
+     * @param {string} cartid cart id
+     * @returns {AxiosPromise} 201 | cart object
+     */
     pickUp (cartid) {
         const data = {
             channel: appConfig.apiChannel
@@ -9,14 +23,26 @@ export default {
 
         return axios.post(appConfig.apiUrl + '/carts/' + cartid, data)
     },
+
     /**
-     * @todo: for single product without variantCode ?
+     * GET | fetch cart by id
+     * @param {string} cartid cart id
      *
-     * @param cartid
-     * @param productCode
-     * @param quantity
-     * @param variantCode
-     * @returns {AxiosPromise}
+     * @returns {AxiosPromise} 200 | cart object
+     */
+    get (cartid) {
+        return axios.get(appConfig.apiUrl + '/carts/' + cartid)
+    },
+
+    /**
+     * POST | add item to cart
+     * @todo: not working for single product without variantCode
+     *
+     * @param {string} cartid cart id
+     * @param {string} productCode product code
+     * @param {number} quantity item quantity
+     * @param {string} variantCode variant code
+     * @returns {AxiosPromise} 201 | cart object
      */
     addItem (cartid, productCode, quantity, variantCode) {
         return axios.post(appConfig.apiUrl + '/carts/' + cartid + '/items', {
@@ -25,15 +51,36 @@ export default {
             variantCode
         })
     },
-    get (cartid) {
-        return axios.get(appConfig.apiUrl + '/carts/' + cartid)
-    },
+
+    /**
+     * PUT | update cart item
+     *
+     * @param {string} cartid cart id
+     * @param {string} itemid item id
+     * @param {number} quantity quantity
+     * @returns {AxiosPromise} 200 | cart object
+     */
     updateItem (cartid, itemid, quantity) {
         return axios.put(appConfig.apiUrl + '/carts/' + cartid + '/items/' + itemid, {quantity})
     },
+
+    /**
+     * DELETE | remove cart item
+     *
+     * @param {string} cartid cart id
+     * @param {string} itemid item id
+     * @returns {AxiosPromise} 200 | cart object
+     */
     removeItem (cartid, itemid) {
         return axios.delete(appConfig.apiUrl + '/carts/' + cartid + '/items/' + itemid)
     },
+
+    /**
+     * DELETE | remove cart object
+     *
+     * @param {string} cartid cart id
+     * @returns {AxiosPromise} 204 | no content
+     */
     clear (cartid) {
         return axios.delete(appConfig.apiUrl + '/carts/' + cartid)
     }
