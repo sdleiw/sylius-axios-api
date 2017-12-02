8300df44040a433e253b35e9f5ea0709c323ec99
Lei Wang <wang@solutiondrive.de>
2017-12-02 12:49:37 +0100

    add api index

---
+++ src/api/index.js
@@ -0,0 +1,24 @@
+/**
+ * @file Documentaion of promised based sylius shop-api using axios
+ *
+ * @version 0.1
+ * @author sdleiw
+ */
+
+import authApi from './authApi'
+import categoryApi from './categoryApi'
+import cartApi from './cartApi'
+import checkoutApi from './checkoutApi'
+import couponApi from './couponApi'
+import productApi from './productApi'
+import reviewApi from './reviewApi'
+
+export {
+    authApi,
+    categoryApi,
+    cartApi,
+    checkoutApi,
+    couponApi,
+    productApi,
+    reviewApi
+}
